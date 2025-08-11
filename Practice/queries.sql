027bea80793ed332fcd5c430695b20107b07aa1a
swatitiwari5300 <swatitiwari5300@gmail.com>
2025-08-11 23:36:02 +0530

    Update queries.sql

--- Practice/queries.sql
+++ Practice/queries.sql
@@ -1,10 +1,7 @@
 --  Q1. Conditional Update with IF/CASE
 -- Increase loyalty_points by:
-
 -- 50 if total_amount > 5000
-
 -- 20 if total_amount between 2000 and 5000
-
 -- 10 otherwise
 -- for all orders with status = 'Delivered'. */
 
@@ -39,11 +36,8 @@ and p.stock_quantity >= od.quantity;
 
 -- Add a New Column and Set Values Based on Condition
 -- Add a column customer_type in Customers:
-
 -- 'Platinum' if loyalty_points >= 200
-
 -- 'Gold' if loyalty_points >= 100 and < 200
-
 -- 'Silver' otherwise.
 
 alter table Customers add column customer_type varchar(20);
@@ -74,3 +68,42 @@ case
     when stock_quantity < 10 then price * 1.10
     when stock_quantity between 10 and then 19 price * 1.15
     end;
+
+
+-- Categorize Orders by Amount
+-- Add a column order_category to Orders:
+-- 'High Value' if total_amount > 5000
+-- 'Medium Value' if between 2000 and 5000
+-- 'Low Value' otherwise
+
+alter table Orders add column order_category varchar(20);
+
+update Orders
+set order_category = 
+case
+when total_amount > 5000 then 'High Value'
+when total_amount between 2000 and 5000 then 'Medium Value'
+else 'Low Value'
+end;
+
+
+--  Update Status Based on Payment
+-- If order total is 0, set status = 'Cancelled';
+-- If order total < 1000, set status = 'Low Payment';
+-- Else 'Processed'.
+
+update Orders
+set status = 
+case
+    when total_amount = 0 then 'Cancelled'
+    when total_amount < 1000 then 'Low Payment'
+    else 'Processed'
+    end;
+
+
+-- Customer Loyalty Tier Update
+-- Change customer_type in Customers table:
+-- 'Diamond' if loyalty_points > 300
+-- 'Platinum' if > 200 and ≤ 300
+-- 'Gold' if > 100 and ≤ 200
+-- 'Silver' otherwise.
